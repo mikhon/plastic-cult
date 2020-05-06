5aa349dbe17e6d1bbcaa4d225b9713c226ab2d3c
Mikko Honkanen <mikko.2.honkanen@aalto.fi>
2020-05-06 19:47:28 +0300

    initial commit

---
+++ Source/PlasticCult/PlasticCultGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlasticCultGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLASTICCULT_API APlasticCultGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
